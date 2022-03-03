ea8d5fbf9754e17dc7f0867e7341862da38c860e
vakhidovulugbek03 <vulugbekk@gmail.com>
2022-03-03 08:44:58 +0500

    commit

---
+++ js/main.js
@@ -0,0 +1,387 @@
+// 2 masala
+// let a = Number(prompt("a"))
+// let b = Number(prompt("b"))
+// let c = Number(prompt("c"))
+
+// if (a > b && a < c){
+//   console.log(a)
+// }
+// else if (b > a && b < c){
+//   console.log(b)
+// }
+// else if (c > a && c < b){
+//   console.log(b)
+// }
+
+
+// 3 masala
+// let a = Number(prompt("a"))
+// let b = Number(prompt("a"))
+// if (a > b) {
+//   console.log(a)
+// }
+// else if ( b > a) {
+//   console.log(b)
+// }
+// else if (a == b){
+//   console.log(0)
+// }
+
+
+// 5 masala
+// let x = Number(prompt("x"))
+// if ( x >0){
+//   2 * Math.sin(x)
+// }
+// else if (x<= 0){
+//   x-6
+// }
+
+
+// 6 masala
+// let x = Number(prompt("x"))
+// if (x < -2 || x > 2){
+//   x = 2 * x
+// }
+// else {
+//   -3 * x
+// }
+
+
+// 8 masala
+// let a = Number(prompt("a"))
+// let b = Number(prompt("b"))
+// let c = Number(prompt("c"))
+// if (a > b + c) {
+//   console.log("yes")
+// }
+// else if (b > a + c){
+//   console.log("yes")
+// }
+// else if (c > a + b){
+//   console.log("yes")
+// }
+// else{
+//   console.log("no")
+// }
+
+
+// 9 masala
+// let a = Number(prompt("harorat kirgazing:"))
+// if (a < 0 ){
+//   console.log("freezing")
+// }
+// else if (a > 1 && a < 10) {
+//   console.log("very cold")
+// }
+// else if (a > 11 && a < 20) {
+//   console.log("cold")
+// }
+// else if (a > 21 && a < 30) {
+//   console.log("normal")
+// }
+// else if (a > 31 && a < 40) {
+//   console.log("hot")
+// }
+// else if (a > 30) {
+//   console.log("hot")
+// }
+
+
+// 10 masala
+// let a = Number(prompt("a"))
+// if (a > 0 && a % 2 == 1) {
+//   console.log("musbat toq son")
+// }
+// else if (a > 0 && a % 2 == 0) {
+//   console.log("musbat juft son")
+// }
+// else if ((a < 0) && (a % 2 == 0)) {
+//   console.log("manfiy juft son")
+// }
+// else if ((a < 0) && (a % 2 == 1)) {
+//   console.log("manfiy toq son")
+// }
+// else if (a == 0) {
+//   console.log("son 0 ga teng")
+// }
+
+
+// 11 masala
+// let a = Number(prompt("a"))
+// if (a < 100 && a % 2 == 1){
+//   console.log("ikki xonali toq son")
+// }
+// else if (a < 1000 && a % 2 == 0 ){
+//   console.log("ikki xonali juft son")
+// }
+// else if (a < 100 && a % 2 == 1){
+//   console.log("uch xonali juft son")
+// }
+// else if (a < 1000 && a % 2 == 0 ){
+//   console.log("uch xonali juft son")
+// }
+
+
+
+// 12 masala
+// let a = Number(prompt("a"))
+// if(a>0){
+//   console.log(a*a)
+// }
+
+// 16 masala hato
+// let n = Number(prompt("n")) 
+// if (n > 0 && n < 10000 && n > 1000){
+//   let a = n % 10000 
+//   let b = n % 1000
+//   let c = n % 100
+//   let d = n % 10
+//   let j = a + b + c + d
+//   console.log(a)
+// }
+// else if
+
+
+
+
+
+//  FORGA OID MASALALR
+
+// 1 masala togri
+// let n = 5
+// let k = 1
+// for (i=0;i<n;i++) {
+//   console.log(k)
+// }
+
+
+// 2 masala togri
+// let a = 3
+// let b = 10
+// let sum = 0
+// for (i = a;i <= b;i++){
+//   console.log(i)
+//   sum++
+// }
+// console.log(sum)
+
+
+// 3 masala togri
+// let a = 3
+// let b = 10
+// let sum = 0
+// for (i = 9 ; i > a; i--){
+//   console.log(i)
+//   sum++
+// }
+// console.log(sum)
+
+
+
+// 4 masala togri
+// let kg = 500
+// for (let i=1; i <= 10 ; i++) {
+//   console.log(kg * i)
+// }
+
+
+
+// 5 masala togri
+// let kg = 1000;
+// for (let i=1; i <= 10; i+=0.1) {
+//   console.log(kg * i);
+// }
+
+
+// 6 masala
+// let kg = 1000;
+// for (let i=1; i <= 10; i+=1.2) {
+//   console.log(kg * i);
+// }
+
+
+// 7 masala togri
+// let a = 3
+// let b = 10
+// let sum = 0
+// for (let i=a; i < b; i++){
+//   sum += i
+// }
+// console.log(sum)
+
+
+
+// 8 masala togri
+// let a = 3
+// let b = 8
+// let sum = 1
+// for (let i = a; i < b; i++){
+//   sum *= i
+// }
+// console.log(sum)
+
+
+// 9 masala togri
+// let a = 3
+// let b = 10
+// let sum = 1
+// for (let i=a; i < b; i++){
+//   sum += (i ** 2)
+// }
+// console.log(sum)
+
+
+// 11 masala ishlamadi yoki ishladi
+// let n = 4
+// let k = 1
+// let j = 4
+// let sum = 0
+// for (let i = k  ; i <= n ; i++  ) {
+//   console.log(((j+k) ** 2));
+//   k++
+//   sum += (k+j) ** 2
+// }
+// console.log(sum)
+
+
+// 12 masala togri emas 
+// let n = 1.5;
+// let sum = 1;
+// for (let i = 1.1 ; i <= n; i += 0.1) {
+//  sum *= i
+// }
+// console.log(sum)
+
+
+// let n = 4
+// let sum = 1;
+// for (i = 1; i < n ; i+=0.1){
+//     // d += 0.1
+//     sum *= i
+//   }
+
+// console.log(sum)
+
+
+// 13 masala ishlamadi
+// let n = 4
+// for (let i = 1 ; i < n; i+= 0.1) {
+//   console.log(i-i)
+// }
+
+
+// 14 masala togri
+// let n = 5
+// for (let i = 1 ; i <= n; i += 2 ) {
+//   console.log(i ** 2)
+// }
+
+
+// 15 masala togri 
+// let a = 4
+// let n = 3
+// let sum = 1
+// for (let i = 1 ; i <= n; i++ ) {
+//   sum *= n
+// }
+// console.log(sum)
+
+
+// 16 masala togri
+// let a = 2
+// let n = 4
+// for (let i = 1 ; i <= n; i++ ) {
+//   console.log(i ** n)
+// }
+
+
+// 17 masala togri 
+// let a = 2
+// let n = 4
+// let sum = 0
+// for (let i = 1 ; i <= n; i++ ) {
+//   console.log(a ** i)
+//   sum += a ** i
+// }
+// console.log(sum)
+
+
+
+
+
+// 19 masala togri
+// let n = 5
+// let sum = 1
+// for (let i =1 ; i <= n; i++) {
+//   sum *= i
+// }
+// console.log(sum)
+
+
+
+// 20 masala togri faktorial
+// let n = 5
+// let sum = 1
+// for (let i =1 ; i <= n; i++) {
+//   sum *= i
+// }
+// console.log(sum)
+
+
+// 21 masala ishladi lekin tekshir
+// let n = 5
+// let sum = 1
+// for (let i =1 ; i <= n; i++) {
+//   sum *= 1 / i
+// }
+// console.log(sum)
+
+
+// 36 masala togri lekin chunmadim
+let n = 5
+let c = 0
+for (let i = 1 ; i <= n ; i++) {
+  let k = 2
+  for (j = 1 ; j <= n ; j++){
+    k *= j
+  }
+  c += k
+}
+console.log(c)
+
+
+
+
+
+// 37 masala ishlamadi
+// let n = 5
+// let sum = 1
+// for (let i = 1 ; i <= n ; i++) {
+//   sum += i
+//   for (let j = 1 ; j <= n ; j++) {
+//     sum ** j
+//   }
+// }
+// console.log(sum)
+
+// for (let i = 1 ; i <= n ; i++) {
+//   let k = 2
+//   for (j = 1 ; j <= n ; j++){
+//     n *= j
+//   }
+//   c += k
+// }
+// console.log(c)
+
+
+
+
+// hato
+// let n = 5
+// let k = 5
+// for (let i = 1 ; i <= n; i++) {
+//   for ( let j = 1; j <= k;j++ ){
+
+//   }
+// }
